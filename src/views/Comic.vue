6af170fa87b9b1d9c4f8e1cdd05af3e9aacc3f48
andreasvirkus <andreasvirkus@gmail.com>
2018-11-05 19:32:25 +0200

    :sparkles: Add initial component structure and call comic request

---
+++ src/views/Comic.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <img :src="src"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  props: {
+    id: Number
+  },
+  data () {
+    return {
+      src: '',
+      alt: ''
+    }
+  },
+  async created () {
+    const onLocalhost = window.location.hostname === 'hostname'
+    const host = onLocalhost ? process.env.API_HOST : ''
+    const res = await fetch(`${host}/.netlify/functions/comic?id=${this.id || 100}`)
+    console.log('comic res:', res)
+  }
+}
+</script>
