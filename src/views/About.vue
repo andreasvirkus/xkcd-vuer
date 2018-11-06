3efeb262d9b10376e43b2bbf4d79852c0192fe6d
andreasvirkus <andreasvirkus@gmail.com>
2018-11-06 10:02:01 +0200

    Fix links on about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,16 +2,16 @@
   <main class="about">
     <h1>:about</h1>
 
-    <p>The comics showed on this page are property of [xkcd](https://xkcd.com).</p>
+    <p>The comics showed on this page are property of <a href="https://xkcd.com">xkcd</a>.</p>
     <p>All credits go to him. I'm just a fan :3</p>
 
     <p>This page is a playground to fiddle with serverless and Go.</p>
     <p>I discovered that xkcd served a json version of its lovely comic strips
     and decided that'd be a lovely thing to play around with.</p>
-    <p>This page makes use of [Vue.js](https://vuejs.org), [Go](https://golang.org/) (soon)
-      & [Netlify](https://netlify.com) for both the static
-      hosting and [Netlify functions](https://www.netlify.com/docs/functions/)
-      (a wrapper for [AWS Lambda](https://aws.amazon.com/lambda/)'s serverless functions).</p>
+    <p>This page makes use of <a href="https://vuejs.org">vue.js</a>, <a href="https://golang.org">Golang</a> (soon)
+      & <a href="https://netlify.com">Netlify</a> for both the static
+      hosting and <a href="https://www.netlify.com/docs/functions/">Netlify functions</a>
+      (a wrapper for <a href="https://aws.amazon.com/lambda/">AWS Lambda</a>'s serverless functions).</p>
   </main>
 </template>
 
