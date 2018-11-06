3f6dcc35b42ec0cdc01c1d43428d453cd683a3d3
andreasvirkus <andreasvirkus@gmail.com>
2018-11-06 21:56:05 +0200

    Add security headers

--- vue.config.js
+++ vue.config.js
@@ -11,5 +11,21 @@ module.exports = {
     assetsVersion: process.env.VUE_APP_VERSION,
     themeColor: '#222222',
     msTileColor: '#222222'
+  },
+  productionSourceMap: false,
+  configureWebpack: {
+    devtool: false,
+    // Prevent webpack from injecting eval / new Function through global polyfill
+    node: {
+      global: false
+    }
+  },
+  chainWebpack: config => {
+    config.plugins
+      .delete('split-manifest')
+      .delete('inline-manifest')
+      // TODO: Fine-tune preloaded & prefetched assets
+      .delete('prefetch')
+      // .delete('preload')
   }
 }
