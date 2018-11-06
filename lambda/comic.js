cca18b288e4d0f7adab70a2169f55553b32789ca
andreasvirkus <andreasvirkus@gmail.com>
2018-11-06 07:27:14 +0200

    :bug: :rocket:  Fix serverless function not bundling dependencies

--- lambda/comic.js
+++ lambda/comic.js
@@ -1,15 +1,13 @@
-const got = require('got')
+import fetch from 'node-fetch'
 
 exports.handler = async (event, context, callback) => {
-  console.log('quack', event)
-  // TODO: Get query param (ID)
-  const id = 616
-  const res = await got(`https://xkcd.com/${id}/info.0.json`, { json: true })
-  console.log('got pkg response', res)
-  const body = await res.body
+  const id = event.queryStringParameters.id
+  const comic = id ? `/${id}` : ''
+  const res = await fetch(`https://xkcd.com${comic}/info.0.json`)
+  const body = await res.json()
 
   callback(null, {
     statusCode: 200,
-    body
+    body: JSON.stringify(body)
   })
 }
