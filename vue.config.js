9361aafa8c5df1da5c56e4bd76f335ae4b8ea0a6
andreasvirkus <andreasvirkus@gmail.com>
2018-11-06 09:20:44 +0200

    Add skipWaiting flag to PWA options, bust cache based on npm ver

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,15 @@
+process.env.VUE_APP_VERSION = require('./package.json').version
+
 module.exports = {
   devServer: {
     proxy: 'http://localhost:9000'
+  },
+  pwa: {
+    workboxOptions: {
+      skipWaiting: true
+    },
+    assetsVersion: process.env.VUE_APP_VERSION,
+    themeColor: '#222222',
+    msTileColor: '#222222'
   }
 }
